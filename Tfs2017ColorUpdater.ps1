2e97a0179a4cd9eefcdfd40b3880d483fc0562bb
leonj <leon.jalfon@checkmarx.com>
2017-11-07 18:25:11 +0200

    Clean Code

--- Tfs2017ColorUpdater.ps1
+++ Tfs2017ColorUpdater.ps1
@@ -1,11 +1,15 @@
-﻿$AppThemesDefaultPath="C:\Program Files\Microsoft Team Foundation Server 15.0\Application Tier\Web Services\_static\tfs\Dev15.M125.1\App_Themes"
+﻿
+#=================================================== PARAMETER ===================================================
 
-if(-not (Test-Path $AppThemesDefaultPath))
+$AppThemesDefaultPath="C:\Program Files\Microsoft Team Foundation Server 15.0\Application Tier\Web Services\_static\tfs\Dev15.M125.1\App_Themes"
+
+#=================================================== FUNCTIONS ===================================================
+
+
+function Update-CssFiles
 {
-    Write-Host "Error, Folder {$AppThemesDefaultPath} not found" -ForegroundColor Red
-}
-else
-{
+    Write-Host "Updating css files..."
+
     $CssFiles = Get-ChildItem -Recurse -Path $AppThemesDefaultPath | Where-Object { $_.Extension -eq ".css"}
 
     foreach($file in $CssFiles)
@@ -16,68 +20,93 @@ else
         (Get-Content $FilePath) -replace "#005a9e","#AC000A" | out-file $FilePath
         (Get-Content $FilePath) -replace "#106ebe","#D7000C" | out-file $FilePath
     }
+
+    Write-Host "" 
+    Write-Host "Files successfully updated!" -ForegroundColor Yellow
 }
 
-Write-Host "Files successfully updated!" -ForegroundColor Green
+
+function Stop-TFS
+{
+    Write-Host ""
+    Write-Host "" 
+    Write-Host "Restarting TFS, Shuting Down..."
 
 
-Write-Host ""
-Write-Host "" 
-Write-Host "Restarting TFS, Shuting Down..."
+
+    # Setup the Process startup info
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+    $pinfo.FileName = "C:\Program Files\Microsoft Team Foundation Server 15.0\Tools\TfsServiceControl.exe"
+    $pinfo.Arguments = "quiesce"
+    $pinfo.UseShellExecute = $false
+    $pinfo.CreateNoWindow = $true
+    $pinfo.RedirectStandardOutput = $true
+    $pinfo.RedirectStandardError = $true
+    $pinfo.WorkingDirectory = Get-Location
+
+    # Create a process object using the startup info
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $pinfo
+
+    # Start the process
+    $process.Start() | Out-Null
+    $process.WaitForExit()
 
 
-
-# Setup the Process startup info
-$pinfo = New-Object System.Diagnostics.ProcessStartInfo
-$pinfo.FileName = "C:\Program Files\Microsoft Team Foundation Server 15.0\Tools\TfsServiceControl.exe"
-$pinfo.Arguments = "quiesce"
-$pinfo.UseShellExecute = $false
-$pinfo.CreateNoWindow = $true
-$pinfo.RedirectStandardOutput = $true
-$pinfo.RedirectStandardError = $true
-$pinfo.WorkingDirectory = Get-Location
-
-# Create a process object using the startup info
-$process = New-Object System.Diagnostics.Process
-$process.StartInfo = $pinfo
-
-# Start the process
-$process.Start() | Out-Null
-$process.WaitForExit()
+    Write-Host $process.StandardOutput.ReadToEnd() -ForegroundColor Gray
+    Write-Host "TFS Stopped!" -ForegroundColor Yellow
+}
 
 
-Write-Host $process.StandardOutput.ReadToEnd() -ForegroundColor Gray
-Write-Host "TFS Stopped!" -ForegroundColor Yellow
+function Start-TFS
+{
+    Write-Host ""
+    Write-Host "" 
+    Write-Host "Restarting TFS, Starting TFS..."
 
 
-Write-Host ""
-Write-Host "" 
-Write-Host "Restarting TFS, Starting TFS..."
+    # Setup the Process startup info
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+    $pinfo.FileName = "C:\Program Files\Microsoft Team Foundation Server 15.0\Tools\TfsServiceControl.exe"
+    $pinfo.Arguments = "unquiesce"
+    $pinfo.UseShellExecute = $false
+    $pinfo.CreateNoWindow = $true
+    $pinfo.RedirectStandardOutput = $true
+    $pinfo.RedirectStandardError = $true
+    $pinfo.WorkingDirectory = Get-Location
+
+    # Create a process object using the startup info
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $pinfo
+
+    # Start the process
+    $process.Start() | Out-Null
+    $process.WaitForExit()
 
 
-# Setup the Process startup info
-$pinfo = New-Object System.Diagnostics.ProcessStartInfo
-$pinfo.FileName = "C:\Program Files\Microsoft Team Foundation Server 15.0\Tools\TfsServiceControl.exe"
-$pinfo.Arguments = "unquiesce"
-$pinfo.UseShellExecute = $false
-$pinfo.CreateNoWindow = $true
-$pinfo.RedirectStandardOutput = $true
-$pinfo.RedirectStandardError = $true
-$pinfo.WorkingDirectory = Get-Location
-
-# Create a process object using the startup info
-$process = New-Object System.Diagnostics.Process
-$process.StartInfo = $pinfo
-
-# Start the process
-$process.Start() | Out-Null
-$process.WaitForExit()
+    Write-Host $process.StandardOutput.ReadToEnd() -ForegroundColor Gray
+    Write-Host "TFS Started!" -ForegroundColor Yellow
+}
 
 
-Write-Host $process.StandardOutput.ReadToEnd() -ForegroundColor Gray
-Write-Host "TFS Started!" -ForegroundColor Yellow
+#====================================================== INIT =====================================================
 
 
-Write-Host ""
-Write-Host "" 
-Write-Host "Process successfully finished!" -ForegroundColor Green
+if(-not (Test-Path $AppThemesDefaultPath))
+{
+    Write-Host "Error, Folder {$AppThemesDefaultPath} not found" -ForegroundColor Red
+}
+else
+{
+    Update-CssFiles
+    Stop-TFS
+    Start-TFS
+
+    Write-Host ""
+    Write-Host "" 
+    Write-Host "Process successfully finished!" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "" 
+}
+
+#=================================================================================================================
